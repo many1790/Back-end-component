83607a8bbfef7642d41ec3815f904a3e6796ebf0
IOSF <om2figueroa@gmail.com>
2026-01-02 20:15:20 +0100

    global fix

--- BACKEND-1949/src/App.js
+++ BACKEND-1949/src/App.js
@@ -1,9 +1,46 @@
-import express from "express";
-import cors from "cors";
-
+const express = require("express");
+const cors = require("cors");
+const {userList}= require("./config/MockedUsers.js");
+const PORT = 3000;
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
-export default app;
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods:["GET","POST","OPTIONS"],
+    allowedHeaders:["Content-Type"],
+}));
+
+//////////////////////////////////////////////////////////
+///////------------enviar un objeto----------------------
+app.get("/users", (req, res)=>{
+    res.send(JSON.stringify(userList))
+})
+///---------enpoints------------------------------------
+app.post("/login", (req,res)=>{
+    const user = userList.find((u) => u.email === email && u.pass === pass);
+    console.log(user);
+
+
+    if(!user){
+        return res.status(200).json({
+            statu:"success",
+            message:"usuario o contraseña incorrectos",
+        })
+    }
+    if(user.role !== "admin"){
+        return res.status(200).json({
+            statu:"success",
+            message:"Faltan credenciales",
+        })
+    }
+    res.status(200).json({
+        statu:"success",
+        userData: user,
+    })
+})
+////////////////////////////
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
